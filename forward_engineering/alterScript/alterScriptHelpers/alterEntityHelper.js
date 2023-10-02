aec68de66f07812767a97c3327377526bfcfb729
unknown <unknown>
2023-10-02 09:24:59 +0300

    - Copied types from PostgreSQL - Started refactoring alter script

---
+++ forward_engineering/alterScript/alterScriptHelpers/alterEntityHelper.js
@@ -0,0 +1,147 @@
+const { checkFieldPropertiesChanged } = require('../../utils/general')();
+
+const getAddCollectionScript =
+	({ app, dbVersion, modelDefinitions, internalDefinitions, externalDefinitions }) =>
+	collection => {
+		const _ = app.require('lodash');
+		const { getEntityName } = require('../../utils/general')(_);
+		const { createColumnDefinitionBySchema } = require('./createColumnDefinition')(app);
+		const ddlProvider = require('../../ddlProvider/ddlProvider')(null, null, app);
+		const { getDefinitionByReference } = app.require('@hackolade/ddl-fe-utils');
+
+		const schemaName = collection.compMod.keyspaceName;
+		const schemaData = { schemaName, dbVersion };
+		const jsonSchema = { ...collection, ...(_.omit(collection?.role, 'properties') || {}) };
+		const columnDefinitions = _.toPairs(jsonSchema.properties).map(([name, column]) => {
+			const definitionJsonSchema = getDefinitionByReference({
+				propertySchema: column,
+				modelDefinitions,
+				internalDefinitions,
+				externalDefinitions,
+			});
+
+			return createColumnDefinitionBySchema({
+				name,
+				jsonSchema: column,
+				parentJsonSchema: jsonSchema,
+				ddlProvider,
+				schemaData,
+				definitionJsonSchema,
+			});
+		});
+		const checkConstraints = (jsonSchema.chkConstr || []).map(check =>
+			ddlProvider.createCheckConstraint(ddlProvider.hydrateCheckConstraint(check)),
+		);
+		const tableData = {
+			name: getEntityName(jsonSchema),
+			columns: columnDefinitions.map(data => ddlProvider.convertColumnDefinition(data)),
+			checkConstraints: checkConstraints,
+			foreignKeyConstraints: [],
+			schemaData,
+			columnDefinitions,
+		};
+		const hydratedTable = ddlProvider.hydrateTable({ tableData, entityData: [jsonSchema], jsonSchema });
+
+		return ddlProvider.createTable(hydratedTable, jsonSchema.isActivated);
+	};
+
+const getDeleteCollectionScript = app => collection => {
+	const _ = app.require('lodash');
+	const { getEntityName, getNamePrefixedWithSchemaName } = require('../../utils/general')(_);
+
+	const jsonData = { ...collection, ...(_.omit(collection?.role, 'properties') || {}) };
+	const tableName = getEntityName(jsonData);
+	const schemaName = collection.compMod.keyspaceName;
+	const fullName = getNamePrefixedWithSchemaName(tableName, schemaName);
+
+	return `DROP TABLE ${fullName};`;
+};
+
+const getAddColumnScript =
+	({ app, dbVersion, modelDefinitions, internalDefinitions, externalDefinitions }) =>
+	collection => {
+		const _ = app.require('lodash');
+		const { getEntityName, getNamePrefixedWithSchemaName } = require('../../utils/general')(_);
+		const { createColumnDefinitionBySchema } = require('./createColumnDefinition')(app);
+		const ddlProvider = require('../../ddlProvider/ddlProvider')(null, null, app);
+		const { getDefinitionByReference } = app.require('@hackolade/ddl-fe-utils');
+
+		const collectionSchema = { ...collection, ...(_.omit(collection?.role, 'properties') || {}) };
+		const tableName = getEntityName(collectionSchema);
+		const schemaName = collectionSchema.compMod?.keyspaceName;
+		const fullName = getNamePrefixedWithSchemaName(tableName, schemaName);
+		const schemaData = { schemaName, dbVersion };
+
+		return _.toPairs(collection.properties)
+			.filter(([name, jsonSchema]) => !jsonSchema.compMod)
+			.map(([name, jsonSchema]) => {
+				const definitionJsonSchema = getDefinitionByReference({
+					propertySchema: jsonSchema,
+					modelDefinitions,
+					internalDefinitions,
+					externalDefinitions,
+				});
+
+				return createColumnDefinitionBySchema({
+					name,
+					jsonSchema,
+					parentJsonSchema: collectionSchema,
+					ddlProvider,
+					schemaData,
+					definitionJsonSchema,
+				});
+			})
+			.map(data => ddlProvider.convertColumnDefinition(data))
+			.map(script => `ALTER TABLE ${fullName} ADD (${script});`);
+	};
+
+const getDeleteColumnScript = app => collection => {
+	const _ = app.require('lodash');
+	const { getEntityName, getNamePrefixedWithSchemaName, wrapInQuotes } = require('../../utils/general')(_);
+	const collectionSchema = { ...collection, ...(_.omit(collection?.role, 'properties') || {}) };
+	const tableName = getEntityName(collectionSchema);
+	const schemaName = collectionSchema.compMod?.keyspaceName;
+	const fullName = getNamePrefixedWithSchemaName(tableName, schemaName);
+
+	return _.toPairs(collection.properties)
+		.filter(([name, jsonSchema]) => !jsonSchema.compMod)
+		.map(([name]) => `ALTER TABLE ${fullName} DROP COLUMN ${wrapInQuotes(name)};`);
+};
+
+const getModifyColumnScript = app => collection => {
+	const _ = app.require('lodash');
+	const { getEntityName, getNamePrefixedWithSchemaName, wrapInQuotes } = require('../../utils/general')(_);
+
+	const collectionSchema = { ...collection, ...(_.omit(collection?.role, 'properties') || {}) };
+	const tableName = getEntityName(collectionSchema);
+	const schemaName = collectionSchema.compMod?.keyspaceName;
+	const fullName = getNamePrefixedWithSchemaName(tableName, schemaName);
+
+	const renameColumnScripts = _.values(collection.properties)
+		.filter(jsonSchema => checkFieldPropertiesChanged(jsonSchema.compMod, ['name']))
+		.map(
+			jsonSchema =>
+				`ALTER TABLE ${fullName} RENAME COLUMN ${wrapInQuotes(
+					jsonSchema.compMod.oldField.name,
+				)} TO ${wrapInQuotes(jsonSchema.compMod.newField.name)};`,
+		);
+
+	const changeTypeScripts = _.toPairs(collection.properties)
+		.filter(([name, jsonSchema]) => checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode']))
+		.map(
+			([name, jsonSchema]) =>
+				`ALTER TABLE ${fullName} MODIFY (${wrapInQuotes(name)} ${_.toUpper(
+					jsonSchema.compMod.newField.mode || jsonSchema.compMod.newField.type,
+				)});`,
+		);
+
+	return [...renameColumnScripts, ...changeTypeScripts];
+};
+
+module.exports = {
+	getAddCollectionScript,
+	getDeleteCollectionScript,
+	getAddColumnScript,
+	getDeleteColumnScript,
+	getModifyColumnScript,
+};
