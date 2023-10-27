9c1672446f6efc91371d749d063622118368b6de
Oleksandr Lytovchenko <oleksandr.lytovchenko@hackolade.com>
2023-10-27 19:26:29 +0200

    HCK-4015: wrap creating indexes statements in try/catch or add 'is not exists' depending on db version

--- forward_engineering/ddlProvider/templates.js
+++ forward_engineering/ddlProvider/templates.js
@@ -22,7 +22,7 @@ module.exports = {
 	createForeignKey:
 		'ALTER TABLE ${foreignTable} ADD CONSTRAINT ${name} FOREIGN KEY (${foreignKey}) REFERENCES ${primaryTable} (${primaryKey})${onDelete};',
 
-	createIndex: 'CREATE${indexType} INDEX${name} ON ${tableName}${keys}${options};\n',
+	createIndex: `CREATE$\{indexType} INDEX$\{ifNotExists}$\{name} ON $\{tableName}$\{keys}$\{options};`,
 
 	createView:
 		'CREATE${orReplace}${force}${viewType}${materialized} VIEW${ifNotExists} ${name} ${sharing}${viewProperties}\n\tAS ${selectStatement}',
