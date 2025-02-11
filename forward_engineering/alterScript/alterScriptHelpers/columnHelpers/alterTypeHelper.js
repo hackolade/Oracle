29f628b31d65941264361a77861ec9bc904125cb
chulanovskyi <oleg.chulanovskyi@binary-studio.com>
2025-02-11 19:02:33 +0200

    feat: alter column type

--- forward_engineering/alterScript/alterScriptHelpers/columnHelpers/alterTypeHelper.js
+++ forward_engineering/alterScript/alterScriptHelpers/columnHelpers/alterTypeHelper.js
@@ -6,9 +6,65 @@ const {
 	getNamePrefixedWithSchemaNameForScriptFormat,
 	getEntityName,
 } = require('../../../utils/general');
+const { assignTemplates } = require('../../../utils/assignTemplates');
+const templates = require('../../../ddlProvider/templates');
 
-// TODO: THIS WILL BE CHANGED TO SUPPORT LENGTH, PRECISION, SCALE TYPE ATTRIBUTES
 /**
+ * @param {string} tableName
+ * @param {string} columnName
+ * @param {string} dataType
+ * @param {{
+ *     length?: number,
+ *     scale?: number,
+ *     precision?: number
+ * }} dataTypeProperties
+ * @return string
+ * */
+const alterColumnType = (tableName, columnName, dataType, dataTypeProperties) => {
+	let dataTypeString = dataType;
+	if (dataTypeProperties.length) {
+		dataTypeString += `(${dataTypeProperties.length})`;
+	} else if (dataTypeProperties.precision && dataTypeProperties.scale) {
+		dataTypeString += `(${dataTypeProperties.precision},${dataTypeProperties.scale})`;
+	} else if (dataTypeProperties.precision) {
+		dataTypeString += `(${dataTypeProperties.precision})`;
+	}
+
+	return assignTemplates(templates.alterColumn, {
+		tableName,
+		columnName,
+		dataType: dataTypeString,
+	});
+};
+
+/**
+ * @return {boolean}
+ * */
+const hasLengthChanged = (collection, oldFieldName, currentJsonSchema) => {
+	const oldProperty = collection.role.properties[oldFieldName];
+
+	const previousLength = oldProperty?.length;
+	const newLength = currentJsonSchema?.length;
+	return previousLength !== newLength;
+};
+
+/**
+ * @return {boolean}
+ * */
+const hasPrecisionOrScaleChanged = (collection, oldFieldName, currentJsonSchema) => {
+	const oldProperty = collection.role.properties[oldFieldName];
+
+	const previousPrecision = oldProperty?.precision;
+	const newPrecision = currentJsonSchema?.precision;
+	const previousScale = oldProperty?.scale;
+	const newScale = currentJsonSchema?.scale;
+
+	return previousPrecision !== newPrecision || previousScale !== newScale;
+};
+
+/**
+ * @param {Object} ddlProvider
+ * @param {string} scriptFormat
  * @return {(collection: Object) => AlterScriptDto[]}
  * */
 const getUpdateTypesScriptDtos = (ddlProvider, scriptFormat) => collection => {
@@ -18,13 +74,22 @@ const getUpdateTypesScriptDtos = (ddlProvider, scriptFormat) => collection => {
 	const fullName = getNamePrefixedWithSchemaNameForScriptFormat(scriptFormat)(tableName, schemaName);
 
 	return _.toPairs(collection.properties)
-		.filter(([name, jsonSchema]) => checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode']))
-		.map(
-			([name, jsonSchema]) =>
-				`ALTER TABLE ${fullName} MODIFY (${prepareNameForScriptFormat(scriptFormat)(name)} ${_.toUpper(
-					jsonSchema.compMod.newField.mode || jsonSchema.compMod.newField.type,
-				)});`,
-		)
+		.filter(([name, jsonSchema]) => {
+			const hasTypeChanged = checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode']);
+			if (!hasTypeChanged) {
+				const oldName = jsonSchema.compMod.oldField.name;
+				const isNewLength = hasLengthChanged(collection, oldName, jsonSchema);
+				const isNewPrecisionOrScale = hasPrecisionOrScaleChanged(collection, oldName, jsonSchema);
+				return isNewLength || isNewPrecisionOrScale;
+			}
+			return hasTypeChanged;
+		})
+		.map(([name, jsonSchema]) => {
+			const typeName = jsonSchema.compMod.newField.mode || jsonSchema.compMod.newField.type;
+			const columnName = prepareNameForScriptFormat(scriptFormat)(name);
+			const typeConfig = _.pick(jsonSchema, ['length', 'precision', 'scale']);
+			return alterColumnType(fullName, columnName, typeName, typeConfig);
+		})
 		.map(script => AlterScriptDto.getInstance([script], true, false));
 };
 
--- forward_engineering/ddlProvider/templates.js
+++ forward_engineering/ddlProvider/templates.js
@@ -71,4 +71,6 @@ module.exports = {
 	},
 
 	alterSession: 'ALTER SESSION SET CURRENT_SCHEMA=${schemaName};\n',
+
+	alterColumn: 'ALTER TABLE IF EXISTS ${tableName} MODIFY (${columnName} ${dataType});',
 };
