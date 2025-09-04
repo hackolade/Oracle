712f4142641677d1efbfae7bb86a3ba9f2565941
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2025-09-04 16:33:15 +0300

    HCK-12609: fix schema name for FK referenced table

--- forward_engineering/ddlProvider/ddlProvider.js
+++ forward_engineering/ddlProvider/ddlProvider.js
@@ -280,7 +280,7 @@ module.exports = (baseProvider, options, app) => {
 			const { foreignOnDelete } = customPropertiesForForeignKey(customProperties);
 
 			const foreignKeyStatement = assignTemplates(templates.createForeignKeyConstraint, {
-				primaryTable: getNamePrefixedWithSchemaName(primaryTable, primarySchemaName || schemaData.schemaName),
+				primaryTable: getNamePrefixedWithSchemaName(primaryTable, primarySchemaName),
 				name: name ? `CONSTRAINT ${prepareName(name)}` : '',
 				foreignKey: isActivated ? foreignKeysToString(foreignKeys) : foreignActiveKeysToString(foreignKeys),
 				primaryKey: isActivated ? foreignKeysToString(primaryKeys) : foreignActiveKeysToString(primaryKeys),
@@ -323,7 +323,7 @@ module.exports = (baseProvider, options, app) => {
 			const { foreignOnDelete } = customPropertiesForForeignKey(customProperties);
 
 			const foreignKeyStatement = assignTemplates(templates.createForeignKey, {
-				primaryTable: getNamePrefixedWithSchemaName(primaryTable, primarySchemaName || schemaData.schemaName),
+				primaryTable: getNamePrefixedWithSchemaName(primaryTable, primarySchemaName),
 				foreignTable: getNamePrefixedWithSchemaName(foreignTable, foreignSchemaName || schemaData.schemaName),
 				name: name ? prepareName(name) : '',
 				foreignKey: isActivated ? foreignKeysToString(foreignKeys) : foreignActiveKeysToString(foreignKeys),
