ca76da56f804b36e558a6a36e25e1666e3c9c302
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2025-09-05 12:31:13 +0300

    browser: remove unused methods from api

--- api/fe.js
+++ api/fe.js
@@ -1,17 +1,11 @@
 const { generateScript } = require('../forward_engineering/api/generateScript');
 const { generateViewScript } = require('../forward_engineering/api/generateViewScript');
 const { generateContainerScript } = require('../forward_engineering/api/generateContainerScript');
-const { getDatabases } = require('../forward_engineering/api/getDatabases');
-const { applyToInstance } = require('../forward_engineering/api/applyToInstance');
-const { testConnection } = require('../forward_engineering/api/testConnection');
 const { isDropInStatements } = require('../forward_engineering/api/isDropInStatements');
 
 module.exports = {
 	generateScript,
 	generateViewScript,
 	generateContainerScript,
-	getDatabases,
-	applyToInstance,
-	testConnection,
 	isDropInStatements,
 };
